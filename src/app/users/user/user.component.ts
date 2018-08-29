ea373d30458504c04ff1d6c2a2174524d068e965
anokha777 <anokha777@gmail.com>
2018-08-29 13:40:03 +0530

    routing 1

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {id: number, name: string};
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    }
+
+    this.route.params.subscribe( 
+      (updatedParam: Params) => {
+        this.user.id = updatedParam['id'];
+        this.user.name = updatedParam['name'];
+      }
+    );
+  }
+
+}
